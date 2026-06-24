61e40c30b6d33240d0a915d2739da5b2d53ad189
Shon Camarlinghi <shon.camarlinghi@gmail.com>
2026-06-23 21:49:37 -0700

    C1_W3 classification, sigmoid function, decision boundary

---
+++ C1_W3_decision_boundary.py
@@ -0,0 +1,18 @@
+#Plot the decision boundary for a logistic regression model. This will give you a better sense of what the model is predicting.
+import numpy as np
+%matplotlib widget
+import matplotlib.pyplot as plt
+from lab_utils_common import plot_data, sigmoid, draw_vthresh
+plt.style.use('./deeplearning.mplstyle')
+
+X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1)
+
+fig,ax = plt.subplots(1,1,figsize=(4,4))
+plot_data(X, y, ax)
+
+ax.axis([0, 4, 0, 3.5])
+ax.set_ylabel('$x_1$')
+ax.set_xlabel('$x_0$')
+plt.show()
+
